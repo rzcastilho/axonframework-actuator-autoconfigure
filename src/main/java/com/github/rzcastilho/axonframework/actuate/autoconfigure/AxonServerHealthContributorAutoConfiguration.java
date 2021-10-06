85c12460924541b701371e6b06b35ecf0f208411
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2021-10-06 14:51:23 -0300

    Update bean injection

--- src/main/java/com/github/rzcastilho/axonframework/actuate/autoconfigure/AxonServerHealthContributorAutoConfiguration.java
+++ src/main/java/com/github/rzcastilho/axonframework/actuate/autoconfigure/AxonServerHealthContributorAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.github.rzcastilho.axonframework.actuate.autoconfigure;
 import com.github.rzcastilho.axonframework.actuate.AxonServerHealthIndicator;
 import io.axoniq.axonserver.connector.AxonServerConnection;
 import java.util.Map;
+import org.axonframework.spring.config.AxonConfiguration;
 import org.axonframework.springboot.autoconfig.AxonServerAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.health.CompositeHealthContributorConfiguration;
 import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
@@ -18,16 +19,16 @@ import org.springframework.context.annotation.Configuration;
  * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration Auto-Configuration} for {@link AxonServerHealthIndicator}
  */
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnClass(AxonServerConnection.class)
-@ConditionalOnBean(AxonServerConnection.class)
+@ConditionalOnClass(AxonConfiguration.class)
+@ConditionalOnBean(AxonConfiguration.class)
 @ConditionalOnEnabledHealthIndicator("axonserver")
 @AutoConfigureAfter({AxonServerAutoConfiguration.class})
-public class AxonServerHealthContributorAutoConfiguration extends CompositeHealthContributorConfiguration<AxonServerHealthIndicator, AxonServerConnection> {
+public class AxonServerHealthContributorAutoConfiguration extends CompositeHealthContributorConfiguration<AxonServerHealthIndicator, AxonConfiguration> {
 
     @Bean
     @ConditionalOnMissingBean(name = {"axonServerHealthIndicator", "axonServerHealthContributor"})
-    public HealthContributor axonServerHealthContributor(Map<String, AxonServerConnection> axonServerConnections) {
-        return createContributor(axonServerConnections);
+    public HealthContributor axonServerHealthContributor(Map<String, AxonConfiguration> axonConfigurations) {
+        return createContributor(axonConfigurations);
     }
 
 }
